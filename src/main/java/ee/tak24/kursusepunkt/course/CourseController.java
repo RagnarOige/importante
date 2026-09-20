92811bdbe4b780a9d2688156d8edec34c114115d
RagnarOige <ragnaroige@gmail.com>
2026-09-20 21:28:06 +0300

    style: intentionally break formatting for E1.3

--- src/main/java/ee/tak24/kursusepunkt/course/CourseController.java
+++ src/main/java/ee/tak24/kursusepunkt/course/CourseController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/courses")
-public class CourseController {
+public class CourseController{
 
   private final CourseService courseService;
 
